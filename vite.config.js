ab570e3c5c7961c6f9be5ae1867e7cca7a09c5be
khadidja <144276956+khadidjainfoinfinity@users.noreply.github.com>
2024-06-11 19:01:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/agency-landing-page/', 
+  base: '/react-landing-page/', // Update the base path here
 })
