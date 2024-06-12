f3f164ca66432bb8fdc4d0f8058f0afd1fec0a76
khadidja <144276956+khadidjainfoinfinity@users.noreply.github.com>
2024-06-12 16:10:51 +0100

    Update services.jsx

--- src/services.jsx
+++ src/services.jsx
@@ -18,37 +18,38 @@ const Services = () => {
   }, []);
 
   return (
+   
     <section className="service" id="services">
       <h1>Our Services</h1>
       <div className="box">
         <div className="item tilt">
           <h2>Community Management</h2>
-          <img src="https://i.postimg.cc/W37Fqsc3/chat.png" alt="Community Management" />
+          <img src="public/chat.png" alt="Community Management" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Affiliate Marketing</h2>
-          <img src={emojiHappyImage} alt="Affiliate Marketing" />
+          <img src="public/emoji-very-happy-alt.png" alt="Affiliate Marketing" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Growth Marketing</h2>
-          <img src={megaphoneImage} alt="Growth Marketing" />
+          <img src="public/megaphone.png" alt="Growth Marketing" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Ecommerce</h2>
-          <img src={rocketImage} alt="Ecommerce" />
+          <img src="public/rocket-launch.png" alt="Ecommerce" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>SEO</h2>
-          <img src={starImage} alt="SEO" />
+          <img src="public/star.png" alt="SEO" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Branding</h2>
-          <img src={userGroupImage} alt="Branding" />
+          <img src="public/user-group.png" alt="Branding" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="shape one"></div>
