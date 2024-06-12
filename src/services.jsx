ad9d9578d7f03aee060359aa074080205c48b093
khadidja <144276956+khadidjainfoinfinity@users.noreply.github.com>
2024-06-12 17:36:10 +0100

    Update services.jsx

--- src/services.jsx
+++ src/services.jsx
@@ -1,6 +1,13 @@
 import { useEffect } from 'react';
 import VanillaTilt from 'vanilla-tilt';
 
+import chatImage from './images/chat.png';
+import emojiImage from './images/emoji-very-happy-alt.png';
+import megaphoneImage from './images/megaphone.png';
+import rocketImage from './images/rocket-launch.png';
+import starImage from './images/star.png';
+import userGroupImage from './images/user-group.png';
+
 const Services = () => {
   useEffect(() => {
     const elements = document.querySelectorAll('.tilt');
@@ -23,26 +30,38 @@ const Services = () => {
       <div className="box">
         <div className="item tilt">
           <h2>Community Management</h2>
+          {/* Use imported image variable */}
+          <img src={chatImage} alt="Community Management" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Affiliate Marketing</h2>
+          {/* Use imported image variable */}
+          <img src={emojiImage} alt="Affiliate Marketing" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Growth Marketing</h2>
+          {/* Use imported image variable */}
+          <img src={megaphoneImage} alt="Growth Marketing" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Ecommerce</h2>
+          {/* Use imported image variable */}
+          <img src={rocketImage} alt="Ecommerce" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>SEO</h2>
+          {/* Use imported image variable */}
+          <img src={starImage} alt="SEO" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="item tilt">
           <h2>Branding</h2>
+          {/* Use imported image variable */}
+          <img src={userGroupImage} alt="Branding" />
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet fuga maiores, reprehenderit beatae odit excepturi dicta repellendus</p>
         </div>
         <div className="shape one"></div>
@@ -53,4 +72,3 @@ const Services = () => {
 };
 
 export default Services;
-
